4606665bc74a33ea5defddd347b9a46d4421f52f
rohitrana18 <rohitrana7703@gmail.com>
2025-06-21 13:47:44 +0530

    commit

--- Hello.java
+++ Hello.java
@@ -1,11 +1,15 @@
-    class Hello{
+class Hello
+{
+    public static void main(String a[])
+    {
+        System.out.print("Hello World");
+    }
+}
 
-        public static void main(String[] args)
-            {
-                byte num1 = 7;
-                byte num2 = 266;
-                byte result = num1 + num2;
-                System.out.println(result);
-                System.out.print("Hello World");
-            }
-               }
+    /* Java is platform indenpendent . It's require JVM to run the code on any O.S but JVM itself is platform dependent because JVM is need to be built for particular O.S.
+       Java is object oriented which means everything should be an object and to create object you need class.
+      
+       Java Code ----> Compiler ----> Byte Code ----> JVM(Java Virtual Machine).
+       .java            (Javac)       .class
+
+       JRE(Java Runtime Environment) => JVM + Libraries. 
