2129e6f3bf94e90643ef905d64337f57eea17e11
rohitrana18 <rohitrana7703@gmail.com>
2025-06-21 18:04:05 +0530

    commit

--- Hello.java
+++ Hello.java
@@ -12,4 +12,33 @@ class Hello
        Java Code ----> Compiler ----> Byte Code ----> JVM(Java Virtual Machine).
        .java            (Javac)       .class
 
-       JRE(Java Runtime Environment) => JVM + Libraries. 
+       JRE(Java Runtime Environment) => JVM + Libraries. */
+
+
+
+
+       // +++++++++++++++++++++++++++++++++++++ Variables +++++++++++++++++++++++++++++++++++++++
+
+       // Variable is the place where data is stored during proccessing .
+
+        num = 5 ; // Here 5 is number which is stored in num variable .
+
+        grades = 8.9 ;
+
+        user = Rohit Rana ;
+
+    class Hello1
+    {
+        public static void main(String a[])
+        {
+
+            int num1 = 8;
+            int num2 = 48;
+            int result = num1 + num2 ;
+            System.out.println(result);
+        }
+    }
+        
+        
+        
+        // Java is strongly typed language which means if you want to store data in variables you have to define the type of data before assigning the data . 
