bd3b8d85c4835bbcdd5eb961d9475d9e54268ba6
rohitrana18 <rohitrana7703@gmail.com>
2025-06-21 18:33:40 +0530

    commit

---
+++ Datatype.java
@@ -0,0 +1,34 @@
+/* Datatpes in java are of two types 
+
+(1). Primitive Datatype
+
+i. Integer => int - 4 bytes, long - 8 bytes , short - 2 bytes, byte - 1 byte (-128 to 127).
+
+ii. Float => double - 8 bytes, float - 4 bytes.
+
+iii. Character => char - 2 bytes.
+
+iv. Boolean 
+
+(2). Non-Primitive Datatype
+*/
+
+class Datatype{
+    public static void main(String a[]){
+
+        int num =3763832; // Default value
+        byte b = 127;
+        short s = 671;
+        long l = 531874698478375l;  // explicitly converted 
+
+
+        double d = 74.44827; // Default value
+        float f = 8427.849f; // explicitly converted
+
+        char c = 'r'; // In character you have to put '' this only.
+
+
+        Boolean bool = true; 
+    }
+}
+
