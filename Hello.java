6029b30166eb4e45dd61c1a621bb930f0081c43c
rohitrana18 <rohitrana7703@gmail.com>
2025-06-21 12:50:54 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+    class Hello{
+
+        public static void main(String[] args)
+            {
+                byte num1 = 7;
+                byte num2 = 266;
+                byte result = num1 + num2;
+                System.out.println(result);
+                System.out.print("Hello World");
+            }
+               }
