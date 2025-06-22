04f4b14f9dc4b541d8efde9747a46f0ddd6444f3
rohitrana18 <rohitrana7703@gmail.com>
2025-06-22 19:53:34 +0530

    commit

--- operators.java
+++ operators.java
@@ -2,25 +2,62 @@ class operators{
     public static void main (String a[]){
 
         // Arithmetic operators 
-        int num = 4;
+        // int num = 4;
       
       
-        int num1 = 39;
-        int result = num +num1;
-        int result1 = num -num1;
-        int result2 = num *num1;
-        int result3 = num /num1;
-        int result4 = num %num1;
+        // int num1 = 39;
+        // int result = num +num1;
+        // int result1 = num -num1;
+        // int result2 = num *num1;
+        // int result3 = num /num1;
+        // int result4 = num %num1;
 
-        num = num +1;
-        num +=1;
-        num++; // post increment
-        ++num; // pre increment
-        num--;
+        // num = num +1;
+        // num +=1;
+        // num++; // post increment
+        // ++num; // pre increment
+        // num--;
 
-        int result = num++; // first fetch the result  then perform increment
-        int result = ++num; // first increment the value then fetch the result
-        System.out.println(result);
+        // int result = num++; // first fetch the result  then perform increment
+        // int result = ++num; // first increment the value then fetch the result
+        // System.out.println(result);
+
+
+
+        // Relational Operators
+        
+        int n1 = 37;
+        int n2 = 83;
+
+        boolean resulta = n1 >n2;
+        boolean resultb = n1< n2;
+        boolean resultc = n1 == n2;
+        boolean resultd = n1 >= n2;
+        boolean resulte = n1 <= n2;
+        boolean resultf = n1 != n2;
+        
+
+
+
+        System.out.println(resulta);
+        System.out.println(resultb);
+        System.out.println(resultc);
+        System.out.println(resultd);
+        System.out.println(resulte);
+        System.out.println(resultf);
+
+
+
+        // Logical operators
+       
+
+
+
+
+
+
+
+        
 
     }
 }
